f49c15eac3e468e9cf40eeeafe11bc6c298a87a9
IhorKurylo <ihorkurylo5@gmail.com>
2023-10-31 20:15:33 +0000

    restore

--- app/Routers/Chatbot.py
+++ app/Routers/Chatbot.py
@@ -32,9 +32,8 @@ def pipeline(value, functions):
 @router.post("/extract_mentioned_data")
 def extract_mentioned_data(url: str = Form(...)):
     start_time = time.time()
-    # functions = [extract_video_id, get_transcript_from_youtube,
-    #              extract_data, complete_profile]
-    functions = [extract_video_id, get_transcript_from_youtube]
+    functions = [extract_video_id, get_transcript_from_youtube,
+                 , complete_profile]
     result = pipeline(url, functions)
     print(result)
     # complete_profile(context)
