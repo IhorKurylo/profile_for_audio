7eb2c22b7c6d6f51a7eb0b90fe8b1a789f9f4a87
IhorKurylo <ihorkurylo5@gmail.com>
2023-11-06 12:41:30 +0000

    static

--- app/Utils/extract_keywords.py
+++ app/Utils/extract_keywords.py
@@ -166,7 +166,7 @@ def get_structured_answer(context: str):
 
 def extract_data(context: str):
     global transcript
-    transcript = context[:100]
+    transcript = context[:1000]
     length = len(context)
     sub_len = 28000
     current = 0
