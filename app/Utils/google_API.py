b1d7410f85befe6b7e86fb3236684a278630c686
IhorKurylo <ihorkurylo5@gmail.com>
2023-11-17 14:34:20 +0000

    map

--- app/Utils/google_API.py
+++ app/Utils/google_API.py
@@ -42,33 +42,36 @@ def get_image_url(search_term):
     except:
         return "https://www.lifespanpodcast.com/content/images/2022/01/Welcome-Message-Title-Card-2.jpg"
 
+
 def get_map_image_url(serach_term):
     try:
         params = {
             "engine": "google_maps",
-            "q": "Granite Paris Haute Restaurant",
-            "ll": "@40.7455096,-74.0083012,15.1z",
+            "q": serach_term,
             "type": "search",
             "api_key": os.getenv("SERP_API_KEY")
         }
-
         search = GoogleSearch(params)
         results = search.get_dict()
-        place_result = results.get('place_results')
-        first_place = place_result
-        if not place_result:
-            print("No place results found.")
+        first_place = results.get('place_results')
+        if not first_place:
             first_place = results.get('local_results')[0]
         if 'gps_coordinates' in first_place:
             lat = first_place['gps_coordinates']['latitude']
             lng = first_place['gps_coordinates']['longitude']
-            # Generate the URL for the static map image
-            map_image_url = f"https://maps.googleapis.com/maps/api/staticmap?center={lat},{lng}&zoom=14&size=400x400&key=YOUR_STATIC_MAPS_API_KEY"
-            print(map_image_url)
-            return map_image_url
+            data_id = first_place['data_id']
+            params = {
+                "engine": "google_maps",
+                "type": "place",
+                "data": f"!4m5!3m4!1s{data_id}!8m2!3d{lat}!4d{lng}",
+                "api_key": os.getenv("SERP_API_KEY")
+            }
+            search = GoogleSearch(params)
+            results = search.get_dict()
+            print(results['search_metadata']['google_maps_url'])
+            # return results['search_metadata']['google_maps_url']
         else:
-            print("No GPS coordinates found for this place.")
+            print("No place results found.")
             return "https://www.google.com/maps/place/Granite/@38.038073,-75.7687759,3z/data=!4m10!1m2!2m1!1sgranite+restaurant+paris!3m6!1s0x47e66f1a1fb579eb:0x265362fbe8c6f7b5!8m2!3d48.8610438!4d2.3419215!15sChhncmFuaXRlIHJlc3RhdXJhbnQgcGFyaXNaGiIYZ3Jhbml0ZSByZXN0YXVyYW50IHBhcmlzkgEXaGF1dGVfZnJlbmNoX3Jlc3RhdXJhbnSaASRDaGREU1VoTk1HOW5TMFZKUTBGblNVTlNYMk54VFRkM1JSQULgAQA!16s%2Fg%2F11ny2076x0?entry=ttu"
     except:
         return "https://www.google.com/maps/place/Granite/@38.038073,-75.7687759,3z/data=!4m10!1m2!2m1!1sgranite+restaurant+paris!3m6!1s0x47e66f1a1fb579eb:0x265362fbe8c6f7b5!8m2!3d48.8610438!4d2.3419215!15sChhncmFuaXRlIHJlc3RhdXJhbnQgcGFyaXNaGiIYZ3Jhbml0ZSByZXN0YXVyYW50IHBhcmlzkgEXaGF1dGVfZnJlbmNoX3Jlc3RhdXJhbnSaASRDaGREU1VoTk1HOW5TMFZKUTBGblNVTlNYMk54VFRkM1JSQULgAQA!16s%2Fg%2F11ny2076x0?entry=ttu"
-        
