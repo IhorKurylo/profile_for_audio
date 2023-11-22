c4fd34cad44ff908692bbe0f66ad65c27812f7e5
IhorKurylo <ihorkurylo5@gmail.com>
2023-11-22 19:49:46 +0000

    update

--- app/Models/Chatbot_Model.py
+++ app/Models/Chatbot_Model.py
@@ -4,9 +4,9 @@ URL_DB = db.urls
 
 def check_already_searched(url):
     result = URL_DB.find_one({"url": url})
-    if result != None:
-      del result['_id']
-    return result
+    if result == None:
+      return result
+    return result['media']
   
 def insert_url_database(url, media):
     result = URL_DB.insert_one({"url": url, "media": media})
