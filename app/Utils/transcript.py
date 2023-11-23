c417b0bb9ed4c3b5519f2ce869af2d03346d6477
goldrace <goldrace@vm1313117.stark-industries.solutions>
2023-11-23 13:42:03 +0000

    update

--- app/Utils/transcript.py
+++ app/Utils/transcript.py
@@ -54,9 +54,10 @@ def get_title_from_youtube(video_id: str):
 
 
 def get_transcript_from_youtube(video_id: str):
-    nltk.download('punkt')
+    # nltk.download('punkt')
     transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
     transcript = ' '.join([segment['text'] for segment in transcript_list])
+    print(transcript)
     sentences = nltk.sent_tokenize(transcript)
     context = ""
     # with open("./data/script.txt", "w") as txt_file:
