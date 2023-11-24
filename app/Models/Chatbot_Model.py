af711b836c848bf70334813b8478097d650412b4
goldrace <goldrace@vm1313117.stark-industries.solutions>
2023-11-24 16:26:07 +0000

    update

--- app/Models/Chatbot_Model.py
+++ app/Models/Chatbot_Model.py
@@ -3,7 +3,7 @@ from app.Database import db
 URL_DB = db.urls
 
 def check_already_searched(url):
-    ChatbotsDB.delete_one({"url": url})
+    URL_DB.delete_one({"url": url})
     result = URL_DB.find_one({"url": url})
     if result == None:
       return result
