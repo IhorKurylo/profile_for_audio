5450443f56c351fa9b1b4201beaf7f7b993c13e9
goldrace <goldrace@vm1313117.stark-industries.solutions>
2023-11-24 16:39:10 +0000

    update

--- app/Models/Chatbot_Model.py
+++ app/Models/Chatbot_Model.py
@@ -3,11 +3,19 @@ from app.Database import db
 URL_DB = db.urls
 
 def check_already_searched(url):
-    URL_DB.delete_one({"url": url})
-    result = URL_DB.find_one({"url": url})
-    if result == None:
-      return result
-    return result['media']
+    # URL_DB.delete_one({"url": url})
+    try:
+      result = URL_DB.find_one({"url": url})
+      if result == None:
+        return result
+      return result['media']
+    except Exception as e:
+      print(e)
+      return None
   
 def insert_url_database(url, media):
-    result = URL_DB.insert_one({"url": url, "media": media})
+    try:
+      result = URL_DB.insert_one({"url": url, "media": media})
+    except Exception as e:
+      print(e)
+      print("insert_url_database error!")
