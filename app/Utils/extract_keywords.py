ad577641ae95d35eec9235d21135ddfe2a393f15
goldrace <goldrace@vm1313117.stark-industries.solutions>
2023-11-29 20:24:12 +0000

    database

--- app/Utils/extract_keywords.py
+++ app/Utils/extract_keywords.py
@@ -152,40 +152,42 @@ def insert_item_to_google_list(item):
         
         
 async def fetch_serp_results(session, query):
-    
-    params = {
-        'api_key': os.getenv("SERP_API_KEY"),      # your serpapi api key: https://serpapi.com/manage-api-key
-        "engine": "google_maps",
-        "type": "search",  # from which device to parse data
-        'q': query, # search query
-    }
-    
-    async with session.get('https://serpapi.com/search.json', params=params) as response:
-        results = await response.json()
-    # print(results)
-
-    first_place = results.get('place_results')
-    if not first_place:
-        first_place = results.get('local_results')[0]
-    if 'gps_coordinates' in first_place:
-        lat = first_place['gps_coordinates']['latitude']
-        lng = first_place['gps_coordinates']['longitude']
-        data_id = first_place['data_id']
+    try:
         params = {
+            'api_key': os.getenv("SERP_API_KEY"),      # your serpapi api key: https://serpapi.com/manage-api-key
             "engine": "google_maps",
-            "type": "place",
-            "data": f"!4m5!3m4!1s{data_id}!8m2!3d{lat}!4d{lng}",
-            "api_key": os.getenv("SERP_API_KEY")
+            "type": "search",  # from which device to parse data
+            'q': query, # search query
         }
         
         async with session.get('https://serpapi.com/search.json', params=params) as response:
             results = await response.json()
+        # print(results)
 
-        map_url = results['search_metadata']['google_maps_url']
-        if substring_to_replace in map_url:
-            map_url = map_url.replace(substring_to_replace, substring_to_replace + f"@{lat},{lng},17z/")
-        serp_result[query] = map_url
-        print(serp_result[query])
+        first_place = results.get('place_results')
+        if not first_place:
+            first_place = results.get('local_results')[0]
+        if 'gps_coordinates' in first_place:
+            lat = first_place['gps_coordinates']['latitude']
+            lng = first_place['gps_coordinates']['longitude']
+            data_id = first_place['data_id']
+            params = {
+                "engine": "google_maps",
+                "type": "place",
+                "data": f"!4m5!3m4!1s{data_id}!8m2!3d{lat}!4d{lng}",
+                "api_key": os.getenv("SERP_API_KEY")
+            }
+            
+            async with session.get('https://serpapi.com/search.json', params=params) as response:
+                results = await response.json()
+
+            map_url = results['search_metadata']['google_maps_url']
+            if substring_to_replace in map_url:
+                map_url = map_url.replace(substring_to_replace, substring_to_replace + f"@{lat},{lng},17z/")
+            serp_result[query] = map_url
+            print(serp_result[query])
+    except:
+        serp_result[query] = "https://www.google.com/maps/place/Granite/@38.038073,-75.7687759,3z/data=!4m10!1m2!2m1!1sgranite+restaurant+paris!3m6!1s0x47e66f1a1fb579eb:0x265362fbe8c6f7b5!8m2!3d48.8610438!4d2.3419215!15sChhncmFuaXRlIHJlc3RhdXJhbnQgcGFyaXNaGiIYZ3Jhbml0ZSByZXN0YXVyYW50IHBhcmlzkgEXaGF1dGVfZnJlbmNoX3Jlc3RhdXJhbnSaASRDaGREU1VoTk1HOW5TMFZKUTBGblNVTlNYMk54VFRkM1JSQULgAQA!16s%2Fg%2F11ny2076x0?entry=ttu"
 
 cx = os.getenv("CX_ID")
 # api_key = os.getenv("GOOGLE_API_KEY")
